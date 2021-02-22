0dceb7ad0ad71eac80b9891bfee22da2f3c36ce3
Francesco Baccetti <gamarantor@gmail.com>
2021-02-22 12:00:31 +0100

    improve(create): add basic scaffolding
    Signed-off-by: Francesco Baccetti <francesco@umaproject.org>

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Claim Dapp</header>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { GlobalStyles } from "twin.macro";
+
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
