3248a663e3c93dea800380af1a36c9f1ceab4470
Jason Kiraly <jaykiraly@gmail.com>
2021-09-29 15:03:57 -0700

    remove risk labs. (#25)
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,7 +40,6 @@ const Footer: React.FC = () => {
           <Link href="/">
             <Logo />
           </Link>
-          <FooterParagraph>Risk Labs © 2020</FooterParagraph>
         </div>
         <div>
           <LinkList>
